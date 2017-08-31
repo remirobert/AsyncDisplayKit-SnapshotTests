ed29708a0ce478e9e8c4f46d5c25c78d7887203a
remi <remirobert33530@gmail.com>
2017-08-31 22:22:33 +0100

    add some tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,29 +1,38 @@
 import UIKit
 import XCTest
-import AsyncDisplayKit-SnapshotTests
+import AsyncDisplayKitSnapshotTests
+import FBSnapshotTestCase
 
-class Tests: XCTestCase {
-    
+class Tests: FBSnapshotTestCase {
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        recordMode = true
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testWithUnlimitedSize() {
+        let node = TextNode(text: "If you support iOS 7 use FBSnapshotTestCase/Core instead")
+        let textNode = node.testNode()
+        FBSnapshotVerifyView(textNode.view)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+
+    func testWithFixWidth() {
+        let node = TextNode(text: "A snapshot test case takes a configured UIView or CALayer and uses the renderInContext: method to get an image snapshot of its contents. It compares this snapshot to a reference image stored in your source code repository and fails the test if the two images don't match.")
+        let textNode = node.testNode(sizeRange: CGSize(width: 100, height: CGFloat.infinity))
+
+        FBSnapshotVerifyView(textNode.view)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testWithFixedHeight() {
+        let node = TextNode(text: "It's straightforward to test logic code, but less obvious how you should test views. You can do a lot of rectangle asserts, but these are hard to understand or visualize. Looking at an image diff shows you exactly what changed and how it will look to users.")
+        let textNode = node.testNode(sizeRange: CGSize(width: CGFloat.infinity, height: 50))
+
+        FBSnapshotVerifyView(textNode.view)
     }
-    
+
+    func testWithDeviceSize() {
+        let node = TextNode(text: "Your unit test must be an application test, not a logic test. (That is, it must be run within the Simulator so that it has access to UIKit.) In Xcode 5 and later new projects only offer application tests, but older projects will have separate targets for the two types.")
+        let textNode = node.testNode(sizeRange: CGSize.phone4_7inch)
+
+        FBSnapshotVerifyView(textNode.view)
+    }
 }
